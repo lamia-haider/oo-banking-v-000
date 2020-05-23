d264d920b73a68095a766a4fa7d81e86f4979fd1
Lamia Haider <lamia.haider@gmail.com>
2020-05-23 04:39:15 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,17 +20,15 @@ class Transfer
 #       each transfer can only happen once (FAILED - 2)
 #       rejects a transfer if the sender doesn't have a valid account (FAILED - 3)
   def execute_transaction
-    if self.valid? == false
-      @status = "rejected"
+
+
+    if self.status != "complete" && self.valid? == true && sender.balance >= amount
+      sender.balance = sender.balance - amount
+      receiver.balance = receiver.balance + amount
+    else self.status = "rejected"
       return "Transaction rejected. Please check your account balance."
     end
 
-    if self.status != "complete" && valid? == true
-      sender.balance = sender.balance - amount
-      receiver.balance = receiver.balance + amount
-      @status = "complete"
-
-    end
 
   end
 
