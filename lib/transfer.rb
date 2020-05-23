42079115cbf8f302ecb14f59ca134584d6571ca6
Lamia Haider <lamia.haider@gmail.com>
2020-05-23 04:26:41 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -24,7 +24,7 @@ class Transfer
       sender.balance = sender.balance - amount
       receiver.balance = receiver.balance + amount
       @status = "complete"
-    else return "Transaction rejected. Please check your account balance."
+    else puts "Transaction rejected. Please check your account balance."
     end
 
   end
