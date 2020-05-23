84e187704a9fcf843f4dc44bebcd04436dba1b5a
Lamia Haider <lamia.haider@gmail.com>
2020-05-23 04:09:50 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,6 +1,6 @@
 class BankAccount
-
-  attr_accessor :name, :balance, :status
+  attr_reader :name
+  attr_accessor :balance, :status
 
   def initialize(name, balance=1000, status="open")
     @name = name
