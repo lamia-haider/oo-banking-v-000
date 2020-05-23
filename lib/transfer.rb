9ee7aff729197c8f8175ab4304ff5ed5a5f75079
Lamia Haider <lamia.haider@gmail.com>
2020-05-23 03:56:03 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -2,6 +2,10 @@ class Transfer
   attr_accessor :sender, :receiver, :status, :amount
 
   def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
   end
 
 
